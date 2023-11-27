3a2becd6f701a929f152b4b6ad2e3301f203f563
Ben Heller <benheller1016@gmail.com>
2023-11-27 11:25:14 -0600

    Added code to create albums table

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+use hippo_ben;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE `albums` (
+                          `id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                          `artist` tinytext NOT NULL,
+                          `name` tinytext NOT NULL,
+                          `release_date` year NOT NULL,
+                          `sales` float(3,1) unsigned NOT NULL,
+  `genre` tinytext NOT NULL
+);
