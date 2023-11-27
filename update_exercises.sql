8d02216523330e983d92e4c07ee4eca5c8d81e5a
Ben Heller <benheller1016@gmail.com>
2023-11-27 16:07:47 -0600

    Completed update exercises

---
+++ update_exercises.sql
@@ -0,0 +1,21 @@
+select name from albums;
+update albums
+set sales = sales * 10;
+select name, sales from albums
+where sales > 100;
+
+select name from albums
+where release_date < 1980;
+update albums
+set release_date = release_date - 100
+where release_date < 1980;
+select name, release_date from albums
+where release_date < 1900;
+
+select name from albums
+where artist = 'Michael Jackson';
+update albums
+set artist = 'Peter Jackson'
+where artist = 'Michael Jackson';
+select artist, name from albums
+where artist = 'Peter Jackson';
