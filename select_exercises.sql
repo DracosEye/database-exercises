9d5347dc6e51d0ec23b089491756b87d7a4f8d22
Ben Heller <benheller1016@gmail.com>
2023-11-27 15:49:59 -0600

    Completed select exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,3 +1,19 @@
 use hippo_ben;
 
 select name from albums
+where artist = 'Pink Floyd';
+
+select release_date from albums
+where name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
+
+select genre from albums
+where name = 'Dangerous';
+
+select name from albums
+where release_date between 1990 and 1999;
+
+select name from albums
+where sales < 20;
+
+select name from albums
+where genre = 'Rock';
