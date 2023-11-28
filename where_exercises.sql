6cb6ad3a8054e2892c70fa8473bd4e8927087a53
Ben Heller <benheller1016@gmail.com>
2023-11-28 15:14:07 -0600

    Completed where clause exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -8,3 +8,15 @@ where last_name like 'E%';
 
 select * from employees
 where last_name like '%q%';
+
+select * from employees
+where (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya') and gender = 'M';
+
+select * from employees
+where last_name like 'E%' or last_name like '%e';
+
+select * from employees
+where last_name like 'E%e';
+
+select * from employees
+where last_name like '%q%' and last_name not like '%qu%';
